ec7808aa2f334e607a6f5a1abc4c572eabf564ac
ptnguyen617 <ptnguyen617@yahoo.com>
2022-09-24 13:43:06 +0900

    test

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-//console.log("connected")
+console.log("connected")
 
+// const maxOfThree = (num1, num2, num3) => {
+//     const array = [num1, num2, num3]
+//     array.sort((a,b)=>{
+//       return b - a
+//     })
+//     return array[0]
+//   }
+
+//   console.log(maxOfThree(12,20,9))
+//   const sumArray = array =>{
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++){
+//       sum += array[i];
+//     }
+//     return sum
+//   }
+//   console.log(sumArray([2,4,5]))
