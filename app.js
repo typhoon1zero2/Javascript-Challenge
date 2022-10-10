0ab179d8cdd0a3987aaee515c18dbc0b684d8b8c
ptnguyen617 <ptnguyen617@yahoo.com>
2022-10-10 22:52:43 +0900

    Removed and added elements with splice

--- app.js
+++ app.js
@@ -178,6 +178,9 @@ Examples of adding/removing elements with splice:
 
 movies => [ 'Caddyshack', 'Interstellar', 'Scarface', 'Trading Places' ]
 let removedMovies = movies.splice(3, 1, 'Spaceballs', 'Alien');
-console.log(movies)
+//console.log(movies)
 movies => [ 'Spaceballs', 'Alien' ]
 removedMovies = movies.splice(1, 0, 'The Sting');
+removedMovies => []
+movies => [ 'Spaceballs', 'The Sting', 'Alien' ]
+console.log(movies)
